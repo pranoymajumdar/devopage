d613fba728d0f93f3bfa916219f52eccd196a06c
Pranoy Majumdar <works.pranoy@gmail.com>
2025-08-14 02:30:25 +0530

    feat: add prisma, better-auth, t3-env

---
+++ src/lib/auth/server.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { env } from "../env";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prismaAdapter, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
+  },
+});
---
+++ src/lib/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.url(),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+  client: {},
+
+  experimental__runtimeEnv: {},
+});
